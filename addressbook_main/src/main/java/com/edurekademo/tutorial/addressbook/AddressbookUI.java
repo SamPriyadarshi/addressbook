a1d95b06ffb74bb881096eaad4c02e8f9e2b7f34
saikatkonar <33577844+saikatkonar@users.noreply.github.com>
2019-06-11 12:13:55 +0530

    Update AddressbookUI.java

--- addressbook_main/src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
+++ addressbook_main/src/main/java/com/edurekademo/tutorial/addressbook/AddressbookUI.java
@@ -1,6 +1,6 @@
 package com.edurekademo.tutorial.addressbook;
 
-import javax.servlet.annotation.WebServlet (failing the build)
+import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
